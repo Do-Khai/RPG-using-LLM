40dc68d3f1ab3b081de8d7727853e691db3da2fb
Hoang Do Khai <khai.hoangdo@ncc.asia>
2025-11-24 11:09:31 +0700

    Update: prompt, retry mechanism

--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
 import httpx
 import os 
 from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 load_dotenv()
 
 OLLAMA_URL = os.getenv("OLLAMA_CLOUD")
 MODEL_NAME = os.getenv("MODEL_NAME")
 
+@retry(
+    retry=retry_if_exception_type((httpx.HTTPStatusError, httpx.TimeoutException)),
+    wait=wait_exponential(multiplier=1, min=2, max=60),
+    stop=stop_after_attempt(5),
+    reraise=True 
+)
 async def call_ollama(payload: dict) -> dict:
     """
     Gửi payload đến Ollama và xử lý response.
