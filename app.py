13be1055e8723204999c0b9c7251871d572262b9
tuyennt550 <trituyen14@gmail.com>
2025-12-12 09:30:37 +0700

    gegeg

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import FastAPI
-from routers import chat_router, battle_router
+from routers import chat_router, battle_router, summary_router
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +13,7 @@ app = FastAPI(title="RPG Quest Proxy Service")
 
 app.include_router(chat_router.router, prefix='/api/v1')
 app.include_router(battle_router.router, prefix='/api/v1')
+app.include_router(summary_router.router, prefix='/api/v1')
 
 @app.get("/health", include_in_schema=False)
 @app.head("/health", include_in_schema=False)
